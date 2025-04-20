19e8621013ab85d63bbdbca150da889474ce2069
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-20 15:10:34 +0200

    extend corpus

--- corpus/Arithmetic.lean
+++ corpus/Arithmetic.lean
@@ -5,6 +5,11 @@ namespace Nat
 
 protected def mod : Nat → Nat → Nat := sorry
 protected def div : Nat → Nat → Nat := sorry
+protected def land : Nat → Nat → Nat := sorry
+protected def lor : Nat → Nat → Nat := sorry
+protected def xor : Nat → Nat → Nat := sorry
+protected def shiftLeft : Nat → Nat → Nat := sorry
+protected def shiftRight : Nat → Nat → Nat := sorry
 
 end Nat
 
@@ -29,3 +34,29 @@ theorem nat_mod₂ : Eq (Nat.mod 1000000000000000000000000000001 100000000000000
 theorem nat_mul₃ : Eq (Nat.mul 1000000000000000 0) 0 := rfl
 
 theorem nat_pow : Eq (Nat.pow 10 15) 1000000000000000 := rfl
+
+theorem nat_beq₁ : Eq (Nat.beq 1000000000000000000000000000000 1000000000000000000000000000001) false := rfl
+
+theorem nat_beq₂ : Eq (Nat.beq 1000000000000000000000000000000 1000000000000000000000000000000) true := rfl
+
+theorem nat_beq₃ : Eq (Nat.beq 1 0) false := rfl
+
+theorem nat_ble₁ : Eq (Nat.ble 1000000000000000000000000000000 1000000000000000000000000000001) true := rfl
+
+theorem nat_ble₂ : Eq (Nat.ble 1000000000000000000000000000002 1000000000000000000000000000001) false := rfl
+
+theorem nat_ble₃ : Eq (Nat.ble 1 0) false := rfl
+
+-- see type_checker.cpp:is_def_eq_zero
+theorem nat_zero_eq_zero : Eq 0 0 := rfl
+
+-- see type_checker.cpp:is_def_eq_succ
+theorem  nat_succ_eq_succ (n : Nat) : Eq (Nat.succ n) (Nat.succ n) := rfl
+
+theorem nat_land : Eq (Nat.land 3 5) 1 := rfl
+
+theorem nat_lor : Eq (Nat.lor 3 5) 7 := rfl
+
+theorem nat_shift_left : Eq (Nat.shiftLeft 1 10) 1024 := rfl
+
+theorem nat_shift_right : Eq (Nat.shiftRight 1024 10) 1 := rfl
